982ac3b2c875563f0e37905a0c6ea6b220f82487
unknown <ljemail@dingtalk.com>
2019-11-29 17:46:27 +0800

    1129

--- fcweb/constant.py
+++ fcweb/constant.py
@@ -1,3 +1,4 @@
+from fcutils import getConfig, getConfigFromConfCenter
 import json
 # 配置文件参数名
 CONF_CENTER_NAME = 'conf_center'
@@ -26,19 +27,19 @@ FC_ENVIRON = 'environ'
 FC_START_RESPONSE = 'start_response'
 
 
-from fcutils import getConfig, getConfigFromConfCenter
-
 _dict = {}
+
 
 def getConfByName(confName):
     ''' 获取配置
-    ''' 
+    '''
     global _dict
     if confName in _dict:
         return _dict[confName]
     elif CONF_CENTER_NAME in _dict:
         confCenter = _dict[CONF_CENTER_NAME]
-        res = getConfigFromConfCenter(confCenter['url'], confName, confCenter['pwd'] )
+        res = getConfigFromConfCenter(
+            confCenter['url'], confName, confCenter['pwd'])
         if res.status_code != 200:
             raise Exception('从配置中心获取密钥失败！')
         data = res.text
@@ -49,20 +50,25 @@ def getConfByName(confName):
         _dict[confName] = data
         return data
     else:
-        raise Exception('')
+        raise Exception('获取配置{}失败'.format(confName))
+
 
 _dict = {}
+
+
 def setEnviron(k, v):
     ''' 设置环境
     '''
     global _dict
     _dict[k] = v
 
+
 def getEnviron(k):
     ''' 获取环境
     '''
     global _dict
     return _dict.get(k, None)
+
 
 def init(environ, start_response):
     ''' 设置环境变量, 设置配置中心url和pwd
@@ -78,8 +84,9 @@ def init(environ, start_response):
         if not confCenter['url'].startswith('http'):
             environ = getEnviron(FC_ENVIRON)
             httpHost = environ['HTTP_HOST'] if 'HTTP_HOST' in environ else environ['REMOTE_ADDR']
-            confCenter['url'] = 'https://{}/2016-08-15/proxy{}'.format(httpHost, confCenter['url'] if confCenter['url'].startswith('/') else '/' + confCenter['url'])
-        
+            confCenter['url'] = 'https://{}/2016-08-15/proxy{}'.format(
+                httpHost, confCenter['url'] if confCenter['url'].startswith('/') else '/' + confCenter['url'])
+
         _dict[CONF_CENTER_NAME] = confCenter
     except Exception as e:
         raise Exception('请在application.py中配置配置中心url和pwd')
